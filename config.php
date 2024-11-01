cd4952b763780e304132e2a960ce05e9f7df17c7
Bobiz2023 <127542727+Bobiz2023@users.noreply.github.com>
2024-11-01 12:36:42 +0100

    Create config.php

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+$servername = "localhost";
+$username = "root"; // اسم المستخدم لقاعدة البيانات
+$password = ""; // كلمة المرور لقاعدة البيانات
+$dbname = "school";
+
+// إنشاء اتصال
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// تحقق من الاتصال
+if ($conn->connect_error) {
+    die("فشل الاتصال بقاعدة البيانات: " . $conn->connect_error);
+}
+?>
